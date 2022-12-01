199a1ac7d342e235d13dfbeb3f72b967fba12740
TacoMaster04 <47572921+TacoMaster04@users.noreply.github.com>
2022-12-02 00:44:38 +0200

    Update purge.js

--- purge.js
+++ purge.js
@@ -1,40 +1,38 @@
-const { getAllMessages } = require("../../modules/transcriptsCreator");
-
 module.exports = {
     execute: async (client, message, args) => {
         if (args[1] == '-nd') {
-            const amount = Number(args[0])
-            if (isNaN(amount) || amount < 1) return message.channel.send(`This command was used incorrectly, please try again.`).then(msg => setTimeout(() => msg.delete(), 5000))
-            let messages = getAllMessages(message.channel)
+            const amount = Number(args[0]);
+            if (isNaN(amount) || amount < 1) return message.channel.send(`This command was used incorrectly, please try again.`).then(msg => setTimeout(() => msg.delete(), 5000));
+            const messages = await channel.messages.fetch({ limit: amount});
             let i = 0;
                 const filteredMessages = [];
-                (await messages).filter((m) => {
+                (messages).filter((m) => {
                     if (m.pinned == false && amount > i) {
                         filteredMessages.push(m);
                         i++
                     }
-                })
-            await message.channel.bulkDelete(filteredMessages, true)
-            await message.channel.send(`\`\`\`diff\nMessages Purged\n- Amount Deleted: ${amount}\n- Requested By: ${message.member.displayName} (${message.author.tag}|${message.author.id})\`\`\``).then(msg => setTimeout(() => msg.delete(), 5000))
+                });
+            await message.channel.bulkDelete(filteredMessages, true);
+            await message.channel.send(`\`\`\`diff\nMessages Purged\n- Amount Deleted: ${amount}\n- Requested By: ${message.member.displayName} (${message.author.tag}|${message.author.id})\`\`\``).then(msg => setTimeout(() => msg.delete(), 5000));
         } else {
-            const amount = Number(args[0])
-            const member = message.mentions.members.first()
-            const messages = getAllMessages(message.channel)
-            if (isNaN(amount) || amount < 1) return message.channel.send(`This command was used incorrectly, please try again.`).then(msg => setTimeout(() => msg.delete(), 5000))
+            const amount = Number(args[0]);
+            const member = message.mentions.members.first();
+            const messages = await channel.messages.fetch({ limit: amount});
+            if (isNaN(amount) || amount < 1) return message.channel.send(`This command was used incorrectly, please try again.`).then(msg => setTimeout(() => msg.delete(), 5000));
             if (member) {
                 let i = 0;
                 const filteredMessages = [];
-                (await messages).filter((m) => {
+                (messages).filter((m) => {
                     if (m.author.id === member.id && amount > i) {
                         filteredMessages.push(m);
                         i++
                     }
-                })
-                await message.channel.bulkDelete(filteredMessages, true)
-                await message.channel.send(`\`\`\`diff\nMessages Purged\n- Amount Deleted: ${filteredMessages.length}\n- Requested By: ${message.member.displayName} (${message.author.tag}|${message.author.id})\n- Member's Messages: ${member.nickname === null ? `No Nickname Set` : member.nickname} (${member.user.username}#${member.user.discriminator}|${member.id})\`\`\``).then(msg => setTimeout(() => msg.delete(), 5000))
+                });
+                await message.channel.bulkDelete(filteredMessages, true);
+                await message.channel.send(`\`\`\`diff\nMessages Purged\n- Amount Deleted: ${filteredMessages.length}\n- Requested By: ${message.member.displayName} (${message.author.tag}|${message.author.id})\n- Member's Messages: ${member.nickname === null ? `No Nickname Set` : member.nickname} (${member.user.username}#${member.user.discriminator}|${member.id})\`\`\``).then(msg => setTimeout(() => msg.delete(), 5000));
             } else {
-                await message.channel.bulkDelete(amount, true)
-                await message.channel.send(`\`\`\`diff\nMessages Purged\n- Amount Deleted: ${amount}\n- Requested By: ${message.member.displayName} (${message.author.tag}|${message.author.id})\`\`\``).then(msg => setTimeout(() => msg.delete(), 5000))
+                await message.channel.bulkDelete(amount, true);
+                await message.channel.send(`\`\`\`diff\nMessages Purged\n- Amount Deleted: ${amount}\n- Requested By: ${message.member.displayName} (${message.author.tag}|${message.author.id})\`\`\``).then(msg => setTimeout(() => msg.delete(), 5000));
             }
         }
     },
