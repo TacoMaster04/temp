7062ec7fbab15cbc325767400d7f4c4fcaa7f30e
TacoMaster04 <47572921+TacoMaster04@users.noreply.github.com>
2022-12-02 00:55:23 +0200

    Update purge.js

--- purge.js
+++ purge.js
@@ -3,7 +3,7 @@ module.exports = {
         if (args[1] == '-nd') {
             const amount = Number(args[0]);
             if (isNaN(amount) || amount < 1 || amount > 100) return message.channel.send(`This command was used incorrectly, please try again.`).then(msg => setTimeout(() => msg.delete(), 5000));
-            const messages = await channel.messages.fetch({ limit: amount });
+            const messages = await message.channel.messages.fetch({ limit: amount });
             let i = 0;
             const filteredMessages = [];
             messages.filter((m) => {
@@ -13,12 +13,12 @@ module.exports = {
                 }
             });
             await message.channel.bulkDelete(filteredMessages, true);
-            await message.channel.send(`\`\`\`diff\nMessages Purged\n- Amount Deleted: ${amount}\n- Requested By: ${message.member.displayName} (${message.author.tag}|${message.author.id})\`\`\``).then(msg => setTimeout(() => msg.delete(), 5000));
+            await message.channel.send(`\`\`\`diff\nMessages Purged\n- Amount Requested: ${amount}\n- Amount Deleted: ${filteredMessages.length}\n- Requested By: ${message.member.displayName} (${message.author.tag}|${message.author.id})\`\`\``).then(msg => setTimeout(() => msg.delete(), 5000));
         } else {
             const amount = Number(args[0]);
             const member = message.mentions.members.first();
             if (isNaN(amount) || amount < 1 || amount > 100) return message.channel.send(`This command was used incorrectly, please try again.`).then(msg => setTimeout(() => msg.delete(), 5000));
-            const messages = await channel.messages.fetch({ limit: amount });
+            const messages = await message.channel.messages.fetch({ limit: amount });
             if (member) {
                 let i = 0;
                 const filteredMessages = [];
@@ -29,10 +29,10 @@ module.exports = {
                     }
                 });
             await message.channel.bulkDelete(filteredMessages, true);
-            await message.channel.send(`\`\`\`diff\nMessages Purged\n- Amount Deleted: ${filteredMessages.length}\n- Requested By: ${message.member.displayName} (${message.author.tag}|${message.author.id})\n- Member's Messages: ${member.nickname === null ? `No Nickname Set` : member.nickname} (${member.user.username}#${member.user.discriminator}|${member.id})\`\`\``).then(msg => setTimeout(() => msg.delete(), 5000));
+            await message.channel.send(`\`\`\`diff\nMessages Purged\n- Amount Requested: ${amount}\n- Amount Deleted: ${filteredMessages.length}\n- Requested By: ${message.member.displayName} (${message.author.tag}|${message.author.id})\n- Member's Messages: ${member.nickname === null ? `No Nickname Set` : member.nickname} (${member.user.username}#${member.user.discriminator}|${member.id})\`\`\``).then(msg => setTimeout(() => msg.delete(), 5000));
             } else {
                 await message.channel.bulkDelete(amount, true);
-                await message.channel.send(`\`\`\`diff\nMessages Purged\n- Amount Deleted: ${amount}\n- Requested By: ${message.member.displayName} (${message.author.tag}|${message.author.id})\`\`\``).then(msg => setTimeout(() => msg.delete(), 5000));
+                await message.channel.send(`\`\`\`diff\nMessages Purged\n- Amount Requested: ${amount}\n- Requested By: ${message.member.displayName} (${message.author.tag}|${message.author.id})\`\`\``).then(msg => setTimeout(() => msg.delete(), 5000));
             }
         }
     },
